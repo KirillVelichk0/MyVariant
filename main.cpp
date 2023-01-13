81dc22279ad17ac7e1b162531902f0337b9d7ab4
Kirill Velichko <cirill@live.ru>
2023-01-14 00:00:34 +0300

    Rewrite type searching to binary search

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <array>
 #include <memory.h>
 #include <cstring>
-template <int curN = 0, class T, class Val, class... Vals>
-constexpr int FindNumber(){
+
+template <std::size_t curN = 0, class T, class Val, class... Vals>
+constexpr std::size_t FindNumber(){
 
     if constexpr(std::is_same_v<std::decay_t<T>, std::decay_t<Val>>){
         return curN;
@@ -39,8 +40,31 @@ constexpr long unsigned int FindMaxSize(){
 template<class... PTypes>
 class MyVariant{
 private:
-    int curT;
+    std::size_t curT;
     std::array<char, FindMaxSize<0, PTypes...>()> data;
+    template <std::size_t curSt, std::size_t minSt, std::size_t maxSt, class Functor>
+    void BinFindAndCall(Functor& f){
+        if constexpr( curSt > 0 && curSt < sizeof...(PTypes)){
+            if(this->curT == curSt){
+                using T = std::tuple_element_t<curSt, std::tuple<PTypes...>>;
+                T data;
+                std::memcpy(&data, &(this->data), sizeof(T));
+                f(data);
+            }
+            else {
+                if(curSt < this->curT)
+                {
+                    constexpr std::size_t nextSt = std::max((curSt + maxSt)/2, curSt + 1);
+                    this->BinFindAndCall<nextSt, curSt + 1, maxSt, Functor>(f);
+                }
+                else {
+                    constexpr std::size_t nextSt = std::min((minSt + curSt) / 2, curSt - 1);
+                    this->BinFindAndCall<nextSt, minSt, curSt - 1, Functor>(f);
+                }
+
+            }
+        }
+    }
     template<int CurSt = 0, class Functor, class T, class... Types>
     void FindAndCall(Functor& f){
         if(this->curT == CurSt){
@@ -70,7 +94,10 @@ public:
     template <class Functor>
     void Call(Functor& f){
         if(this->curT != -1){
-            this->FindAndCall<0, Functor, PTypes...>(f);
+            constexpr std::size_t startSt = 0 + sizeof...(PTypes) - 1;
+
+            this->BinFindAndCall<startSt, 0, sizeof...(PTypes) - 1, Functor>(f);
+
         }
     }
 };
@@ -83,6 +110,8 @@ int main()
     };
     v.Call(pr);
     v = false;
+    v.Call(pr);
+    v= 13;
     v.Call(pr);
 }
 
